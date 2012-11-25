ce520478ca5de3909e3f7a6066c6af5a83b240af
Vitalii Nagara <duke@ejoom.com>
2012-11-25 22:03:04 +0200

    Update composer to new version of modules.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,21 @@
 <?php
 
+/**
+ * Some default configurations
+ */
 date_default_timezone_set('UTC');
 mb_internal_encoding('utf-8');
 
+//ini_set('display_errors', 1); //development
+
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
 chdir(dirname(__DIR__));
 
 // Setup autoloading
-include 'init_autoloader.php';
+require 'init_autoloader.php';
 
 // Run the application!
 Zend\Mvc\Application::init(include 'config/application.config.php')->run();
